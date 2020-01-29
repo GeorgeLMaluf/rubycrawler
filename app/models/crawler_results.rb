d931fb64341f91bb542859b58e5294968184bb31
George L. 'Maverick' Maluf <georgelmaluf286@gmail.com>
2020-01-29 11:33:24 -0300

    Ajustes na apresentaçao do resultado

---
+++ app/models/crawler_results.rb
@@ -0,0 +1,17 @@
+class CrawlerResults    
+    def url
+        @url
+    end
+
+    def url=(nova_url)
+        @url = nova_url
+    end
+
+    def conteudo
+        @resultado
+    end
+
+    def conteudo=(novo_resultado)
+        @resultado = novo_resultado
+    end
+end
